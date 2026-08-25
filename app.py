33070fbe6d6493052c1404dd100d7b9ef2f6b5da
carlos.cantalapiedra <carlos.cantalapiedra@efectoscopio.com>
2026-08-25 16:08:17 +0200

    titulo

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ class DPSCopyVersionApp(Application):
         import dialog as dialog_module
 
         self._dialog = self.engine.show_dialog(
-            "DPS — Copy Version to Envíos",
+            "EFECTOSCOPIO — Copy Version to Envios",
             self,
             dialog_module.AppDialog,
             self,
